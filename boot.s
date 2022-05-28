504569b907355bd34a057fed4e3514e1d9e12d55
Xuni <81808352+xunillen@users.noreply.github.com>
2022-05-28 23:12:55 +0200

    Added some coments in preparations for tomorow work

--- boot.s
+++ boot.s
@@ -2,14 +2,15 @@ movw $0x7c0, %ax
 movw %ax, %ds
 
 loop:
-	movb	$0x07, %ah
-	movb	$0x00, %al
-	movb	$0x07, %bh
-	movb	$0x00, %cl
-	movb	$0x18, %dh
-	movb	$0x4f, %dl
-	int	$0x10
+        movb    $0x07, %ah      # Scroll down window
+        movb    $0x00, %al      # Koliko linija da pomaknemo dolje. (0 za brisanje ekrana)
+        movb    $0x07, %bh
+        movw    $0x00, %cx      # Cursor - 0,0 position (row, colum up left)
+                                # Specificiramo gornji lijevi dio ekrana
+        movb    $0x18, %dh      # Lower right end of screen (14, 70)
+        movb    $0x4f, %dl
 
-	jmp loop
+        int     $0x10
 
+        jmp loop
 
