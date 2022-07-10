0bc9712f06f49447f447746fb4162028aca255a2
Xunillen <xunillen@protonmail.com>
2022-07-10 22:58:36 +0200

    Modified print_text to act as function

--- boot.s
+++ boot.s
@@ -1,5 +1,5 @@
 ################ Memory map (Real Mode) ######################
-## Nmae					Start		End		Size
+## Name					Start		End		Size
 ## REAL MODE INTERRUPT TABLE(IVT)	0x00000000	0x000003FF	1KiB - 	    Unusable ---------------|
 ## BIOS DATA AREA (BDA)			0x00000400	0x000004FF	256 bytes - Unusable ---------------|
 ## CONVECTIONAL MEMORY			0x00000500	0x00007BFF	30 KiB	-------|		    |
@@ -12,31 +12,46 @@
 ## BIOS EXPANSIONS			0x000C8000	0x000EFFFF	160 KiB	- BIOS Expansions ----------|
 ## MOTHERBOARD BIOS			0x000F0000	0x000FFFFF	64 KiB	- Motherboard bios ---------|
 ############################################################
+.code16
 
-movw    $0x7c0, %ax
+
+.globl _start
+.section .text
+
+# Setup segments
+movw    $0x600, %ax
 movw    %ax, %ds
-
 
 movw    $0x7e0, %ax
 movw    %ax, %ss
 
 movw    $0x2000, %sp
 
-# call clear_screen
-
-#.section .data
-#       sample_text:
-#               .ascii "Test\0"
-
 _start:
-        movw    $0x00,  %cx
+	# Clear screen
+	call clear_screen
+	# Print message
+	pushw	$sample_text
+	call print_text
 
-        print_loop:
-                movb    $0x0e,  %ah
+       loop_end:
+		jmp loop_end
 
-                movb    sample_text(,%ecx,1), %al
+print_text:
+	pushw	%bp
+	movw	%sp, %bp
+
+	movw	8(%bp), %dx
+
+	movb	$0x00, %ch
+	movb	$0x00, %cl
+	movw	$0, %cx
+	print_loop:
+	        movb    $0x0e,  %ah
+
+                movb    (%edx,%ecx,1), %al
                 cmpb    $0, %al
-                je      loop_end
+                je      print_end
 
                 movb    $0x00,  %bh
                 movb    $0x07,  %bl
@@ -45,11 +60,10 @@ _start:
 
                 incw    %cx
                 jmp     print_loop
-
-        loop_end:
-                jmp loop_end
-
-
+	print_end:
+		movw	%bp, %sp
+		popw	%bp
+		ret
 
 clear_screen:
         pushw   %bp
@@ -64,11 +78,13 @@ clear_screen:
         movb    $0x18, %dh      # Specificiramo doljni dio ekrana (24, 70). Doljni red i desni kut
         movb    $0x4f, %dl
 
-        movw    %bp, %sp
-        popw    %bp
-
         int     $0x10
 
+	movw	%bp, %sp
+	popw	%bp
+	ret
 
+.section .data
 sample_text:
-        .ascii  "18 naked cowboys in the showers at Ram Ranch!\0"
+        .ascii  "Welcome to Links!\0"
+
