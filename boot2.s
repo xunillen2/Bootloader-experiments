ac3db689d5fc5fd4bb2cc03c40ab15d13d7575a1
Xunillen <xunillen@protonmail.com>
2022-07-21 22:47:07 +0200

    Added copy of error_unsupported and error_reboot in boot2.s until I dont refactor and devide code

--- boot2.s
+++ boot2.s
@@ -79,6 +79,11 @@ a20_status:
                 movw    %bp, %sp
                 popw    %bp
 		ret
+activate_a20_line:
+	
+
+
+
 # Temp print function until i dont move it to seperate file.
 print_text:
         pushw   %bp
@@ -107,9 +112,42 @@ print_text:
                 movw    %bp, %sp
                 popw    %bp
                 ret
+
+## ERROR SUBROUTINES ##
+#
+# ABOUT:
+#       If error while calling bios function is 0x86 or 0x80
+#       jmp to error_unsuported, which will print that function
+#       is not supported, and will continue down to error_reboot.
+#       If carry flag is set, jmp to error_reboot,
+#       which will print message, and wait for user input (int 0x16, ah 0x00),
+#       and it will then reboot the system.
+#
+error_unsupported:
+        pushw   $error_text
+        call    print_text      # Print function error text
+        subw    $2, %sp
+error_reboot:
+        pushw   $error_text
+        call    print_text      # Print error text
+        subw    $2, %sp
+
+        xor     %ax, %ax        # ah - 0x00 and it 0x16 for reading keyboard scancode
+        int     $0x16           # Contines executing after any key on keyboard
+                                # has been pressed
+
+        jmp     $0xffff, $0000  # Jumps to reset vector, and reboots pc
+                                # FFFF * 16 + 0 = FFFF0 ->
+                                #       1048560 - 16 bytes below 1mb
+
 #.section .data
 #debug_msg:
 	a20_line_enabled:
 		.ascii	"A20 Line ENABLED\0"
 	a20_line_disabled:
 		.ascii	"A20 Line ENABLED\0"
+	error_uns_text:
+        	.ascii  "Function not supported, or is invalid.\0"
+	error_text:
+        	.ascii  "Boot error... Press any key to reboot.\0"
+
