425208b74809d9afbc5bad870a9801e36aa0fdf3
xunillen <xunillen@protonmail.com>
2022-10-04 21:49:16 +0200

    Migrated all printing and not needed code from first stage to second stage bootloader.

--- boot.s
+++ boot.s
@@ -70,127 +70,20 @@ main:
 	movb	%dl, boot_drive	# Save value that represents the drive
 				# we booted from
 
-#	call	clear_screen       # Clear screen
 	call	reset_disk         # Reset disk
-
-#        pushw   $copyright	# Print CopyRight message
-#        call    print_text
-
-#	pushw	$welcome_text   # Print welcome message
-#	call	print_text
 
 	fat_init:
 		call	load_fat
 	load_second:
 		pushw	$second_stage_name
 		call    find_file
-		pushw	$0x800
+		pushw	$0x7e0
 		pushw	%cx
 		pushw	%ax
-		call	read_file_linear				
-#	jmp_second:
-#		jmp	0x8000
-#		pushw	$18
-#		call	read_sectors
-#		jc      error_reboot
-#	call	load_second_bt     # Load second bootlaoder
-
+		call	read_file_linear
+		jmp	0x7e00				
 	end:
 		jmp end
-
-######################
-## SCREEN FUNCTIONS ##
-######################
-# INT 10 - 17th int vector
-#	Interrupt handler that BIOS sets up. They provide video services ->
-#	Video mode (0h), cursor position (02h), get cursor position (02h)...
-#
-# ABOUT:
-#	Prints text from given address until null char is hit.
-#       INT $0x10
-#	PARAMETERS:
-#		1. Parameter 1 - Address to data to print	<-- 4(%bp)
-#       REGISTERS:
-#		%ah - 0x0e - function code
-#		%al - Caracter to output
-#		%bh - Page number
-#		%bl - Color
-#		%ax - Printed characters
-#       RETURNS:
-#		Number of writen bytes in %ax
-#       NOTES:
-#
-#print_text:
-#        pushw   %bp
-#        movw    %sp, %bp
-#
-#        movw    4(%bp), %di
-#
-#        xor     %ax, %ax        # Empty counter register
-#                                # Used for counting chars.
-#	movw	%ax, %es
-#
-#        print_loop:
-#                movb    %es:(%di), %al
-#                cmpb    $0, %al
-#                je      print_end
-#
-#		movb    $0x0e,  %ah
-#                movb    $0x00,  %bh
-#                movb    $0x07,  %bl
-#
-#                int     $0x10
-#
-#		incw	%di
-#                jmp     print_loop
-#        print_end:
-#		movw	4(%bp), %ax	# Get original address
-#		subw	%ax, %di	# Sub final address from orig. address
-#		movw	%di, %ax	# Move result to %ax for return value.
-#                movw    %bp, %sp
-#                popw    %bp
-#                ret
-
-#
-# ABOUT:
-#       Clears screen
-#       INT $0x10
-#       PARAMETERS:
-#
-#       REGISTERS:
-#		%ah - 0x6 - Function code to scroll up
-#				(0x7 for down)
-#		%al - Lines to scroll (down or up) if 0 (clear),
-#				then ch, cl, dh, dl are used
-#		%bh - Background color -> high four bits are for background,
-#				and for low are for foregorund
-#				(see BIOS color attributes)
-#		%ch - Upper row number
-#		%cl - Left column number
-#		%dh - Lower row number
-#		%dl - Right column number
-#
-#       RETURNS:
-#
-#       NOTES:
-#
-#clear_screen:
-#        pushw   %bp
-#        movw    %sp, %bp
-
-#        movb    $0x07, %ah
-#        movb	$0x00, %al
-#        movb    $0x07, %bh
-
-#        xor	%cx, %cx
-#        movb    $0x19, %dh
-#        movb    $0x50, %dl
-
-#        int     $0x10
-
-#	movw	%bp, %sp
-#	popw	%bp
-#	ret
 
 ####################
 ## ATA OPERATIONS ##
@@ -219,54 +112,12 @@ reset_disk:
 	movw	%sp, %bp
 
 	xor	%ax, %ax		# For reseting disk system
-	Movb	boot_drive, %dl		# Using 0x80 to use first disk
+	movb	boot_drive, %dl		# Using 0x80 to use first disk
 	int	$0x13
-
-#	jc	error_reboot		# If carry flag is set, error occured
-#	cmpb	$0x86,	%ah		# If %ah is set to 0x86 -> unsupported function
-#	je	error_unsupported
-#	cmpb	$0x80,	%ah		# If %ag is set to 0x80 -> Invalid function
-#	je	error_unsupported
 
 	movw	%bp, %sp
 	popw	%bp
 	ret
 
-## ERROR SUBROUTINES ##
-#
-# ABOUT:
-#	If error while calling bios function is 0x86 or 0x80
-#	jmp to error_unsuported, which will print that function
-#	is not supported, and will continue down to error_reboot.
-#	If carry flag is set, jmp to error_reboot,
-#	which will print message, and wait for user input (int 0x16, ah 0x00),
-#	and it will then reboot the system.
-#
-#error_unsupported:
-#	pushw   $error_uns_text
-#	call    print_text      # Print function error text
-#	subw	$2, %sp
-#error_reboot:
-#	pushw	$error_text
-#	call	print_text	# Print error text
-#	subw	$2, %sp
-
-#	xor	%ax, %ax	# ah - 0x00 and it 0x16 for reading keyboard scancode
-#	int	$0x16		# Contines executing after any key on keyboard
-#				# has been pressed
-#
-#	jmp	$0xffff, $0000	# Jumps to reset vector, and reboots pc
-				# FFFF * 16 + 0 = FFFF0 ->
-				#	1048560 - 16 bytes below 1mb
-#welcome_text:
-#        .ascii  "Welcome to LinksBoot!\n\rBooting...\n\0"
-#copyright:
-#	.ascii	"CopyRight Xunillen. GPL license.\n\r\0"
-error_uns_text:
-#	.ascii  "\n\n\rFunction not supported, or is invalid.\0"
-	.ascii "F\0"
-error_text:
-#	.ascii	"\n\rBoot error... Press any key to reboot.\0"
-	.ascii "E\0"
 second_stage_name:
 	.ascii "LINKSCNDBTT"
--- boot2.s
+++ boot2.s
@@ -4,6 +4,12 @@
 .section .text
 
 _start:
+	startup_messages:
+		call	clear_screen
+		pushw	$copyright
+		call	print_text
+		pushw	$welcome_text
+		call	print_text
 
 	### A20 ###
 	call	a20_status
@@ -375,7 +381,29 @@ enter_protected:
   clear_prefetch_queue:
 	ret
 
-# Temp print function until i dont move it to seperate file.
+
+######################
+## SCREEN FUNCTIONS ##
+######################
+# INT 10 - 17th int vector
+#       Interrupt handler that BIOS sets up. They provide video services ->
+#       Video mode (0h), cursor position (02h), get cursor position (02h)...
+#
+# ABOUT:
+#       Prints text from given address until null char is hit.
+#       INT $0x10
+#       PARAMETERS:
+#               1. Parameter 1 - Address to data to print       <-- 4(%bp)
+#       REGISTERS:
+#               %ah - 0x0e - function code
+#               %al - Caracter to output
+#               %bh - Page number
+#               %bl - Color
+#               %ax - Printed characters
+#       RETURNS:
+#               Number of writen bytes in %ax
+#       NOTES:
+#
 print_text:
         pushw   %bp
         movw    %sp, %bp
@@ -406,6 +434,46 @@ print_text:
                 movw    %bp, %sp
                 popw    %bp
                 ret
+# ABOUT:
+#       Clears screen
+#       INT $0x10
+#       PARAMETERS:
+#
+#       REGISTERS:
+#               %ah - 0x6 - Function code to scroll up
+#                               (0x7 for down)
+#               %al - Lines to scroll (down or up) if 0 (clear),
+#                               then ch, cl, dh, dl are used
+#               %bh - Background color -> high four bits are for background,
+#                               and for low are for foregorund
+#                               (see BIOS color attributes)
+#               %ch - Upper row number
+#               %cl - Left column number
+#               %dh - Lower row number
+#               %dl - Right column number
+#
+#       RETURNS:
+#
+#       NOTES:
+#
+clear_screen:
+        pushw   %bp
+        movw    %sp, %bp
+
+        movb    $0x07, %ah
+        movb    $0x00, %al
+        movb    $0x07, %bh
+
+        xor     %cx, %cx
+        movb    $0x19, %dh
+        movb    $0x50, %dl
+
+        int     $0x10
+
+        movw    %bp, %sp
+        popw    %bp
+        ret
+
 
 ## FILES ##
 list_files:
@@ -439,7 +507,10 @@ error_reboot:
         jmp     $0xffff, $0000  # Jumps to reset vector, and reboots pc
                                 # FFFF * 16 + 0 = FFFF0 ->
                                 #       1048560 - 16 bytes below 1mb
-
+welcome_text:
+        .ascii  "Welcome to LinksBoot!\n\rBooting...\n\0"
+copyright:
+       .ascii  "CopyRight Xunillen. GPL license.\n\r\0"
 a20_line_enabled:
 	.ascii	"\n\rA20 Line ENABLED\0"
 a20_line_disabled:
